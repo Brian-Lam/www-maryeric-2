480a2b10aa23443cbbe7591302699c32787099f8
Brian Lam <brian@brianlam.me>
2017-03-02 17:57:19 -0800

    use template on about page

--- about.php
+++ about.php
@@ -34,39 +34,7 @@
 		</div>
 	</header>
 
-	<div id="fh5co-couple">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-					<h2>Hello!</h2>
-					<h3>Stepmber 3, 2017, Lemont, IL</h3>
-					<p>We invited you to celebrate our wedding</p>
-				</div>
-			</div>
-			<div class="couple-wrap animate-box">
-				<div class="couple-half">
-					<div class="groom">
-						<img src="images/groom.jpg" alt="groom" class="img-responsive">
-					</div>
-					<div class="desc-groom">
-						<h3>Eric Lam</h3>
-						<p>TEXT HERE</p>
-					</div>
-				</div>
-				<p class="heart text-center"><i class="icon-heart2"></i></p>
-				<div class="couple-half">
-					<div class="bride">
-						<img src="images/bride.jpg" alt="groom" class="img-responsive">
-					</div>
-					<div class="desc-bride">
-						<h3>Mary Ducket</h3>
-						<p>TEXT HERE</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
+	<?php include("templates/couple.php"); ?>
 	<?php include("templates/story.php"); ?>
 	<?php include("templates/attending.php"); ?>
 
