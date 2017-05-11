b06974a27ba83088aa559a1529d00d0e49137594
Brian Lam <brian@brianlam.me>
2017-05-10 21:29:33 -0700

    fix missing semicolon

--- rsvp-submit.php
+++ rsvp-submit.php
@@ -46,35 +46,35 @@
     $menu1 = $_POST["menu-1"];
     $shuttle1 = $_POST["shuttle-1"];
     $comments1 = $_POST["comments-1"];
-    $dietary1 = $_POST["dietary-1"]
+    $dietary1 = $_POST["dietary-1"];
 
     $name2 = $_POST["name-2"];
     $attending2 = $_POST["attending-2"];
     $menu2 = $_POST["menu-2"];
     $shuttle2 = $_POST["shuttle-2"];
     $comments2 = $_POST["comments-2"];
-    $dietary2 = $_POST["dietary-2"]
+    $dietary2 = $_POST["dietary-2"];
 
     $name3 = $_POST["name-3"];
     $attending3 = $_POST["attending-3"];
     $menu3 = $_POST["menu-3"];
     $shuttle3 = $_POST["shuttle-3"];
     $comments3 = $_POST["comments-3"];
-    $dietary3 = $_POST["dietary-3"]
+    $dietary3 = $_POST["dietary-3"];
 
     $name4 = $_POST["name-4"];
     $attending4 = $_POST["attending-4"];
     $menu4 = $_POST["menu-4"];
     $shuttle4 = $_POST["shuttle-4"];
     $comments4 = $_POST["comments-4"];
-    $dietary4 = $_POST["dietary-4"]
+    $dietary4 = $_POST["dietary-4"];
 
     $name5 = $_POST["name-5"];
     $attending5 = $_POST["attending-5"];
     $menu5 = $_POST["menu-5"];
     $shuttle5 = $_POST["shuttle-5"];
     $comments5 = $_POST["comments-5"];
-    $dietary5 = $_POST["dietary-5"]
+    $dietary5 = $_POST["dietary-5"];
 
     $stmt->execute();
     $stmt->close();
