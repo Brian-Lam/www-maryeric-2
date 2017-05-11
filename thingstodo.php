564caf1e9a67b334007d4e2b0b0273777874d0c2
Brian Lam <brian@brianlam.me>
2017-05-10 22:27:54 -0700

    open thignstodo links in new tab

--- thingstodo.php
+++ thingstodo.php
@@ -36,13 +36,13 @@
 
 	<div id="fh5co-couple-story">
         <div class="container">
-  While our wedding is sure to be the highlight of your Labor Day weekend, we realize you may want to explore a bit before and/or after the festivities. All of the wedding day events will take place at <a href="http://www.themontebelloestate.com">The Monte Bello Estate</a> in Lemont, about 20 miles southwest of downtown Chicago.
+  While our wedding is sure to be the highlight of your Labor Day weekend, we realize you may want to explore a bit before and/or after the festivities. All of the wedding day events will take place at <a target="_blank" href="http://www.themontebelloestate.com">The Monte Bello Estate</a> in Lemont, about 20 miles southwest of downtown Chicago.
 <br />
 &nbsp;
 <br />
 To help you plan your weekend, we&rsquo;ve included some ideas for places to check out and places to eat, both in the Lemont area and in Chicago.
 <br />
-For more information and recommendations, check out <a href="http://www.choosechicago.com">Choose Chicago</a> and <a href="https://www.enjoyillinois.com/en-us">Enjoy Illinois</a>
+For more information and recommendations, check out <a target="_blank" href="http://www.choosechicago.com">Choose Chicago</a> and <a target="_blank" href="https://www.enjoyillinois.com/en-us">Enjoy Illinois</a>
 <br />
 &nbsp;
 <br />
@@ -51,26 +51,26 @@ For more information and recommendations, check out <a href="http://www.choosech
 <h4>What to Eat</h4>
     You won&rsquo;t go hungry at our wedding, so bring your appetite! If you are exploring the area on Saturday or during the day on Sunday, here are a few local favorite places to eat:
 <br />
-&bull; <a href="http://www.vaultcafeandbar.com">Vault Caf&eacute; and Bar</a> serves American fare and is housed in a historic bank building in downtown Lemont
+&bull; <a target="_blank" href="http://www.vaultcafeandbar.com">Vault Caf&eacute; and Bar</a> serves American fare and is housed in a historic bank building in downtown Lemont
 <br />
-&bull; <a href="http://www.oldtownrestaurant.com/index.html">Old Town Restaurant</a> in Lemont is a family-owned eatery specializing in hearty Polish and Eastern European comfort foods (such as goulash, potato pancakes, and blintzes)
+&bull; <a target="_blank" href="http://www.oldtownrestaurant.com/index.html">Old Town Restaurant</a> in Lemont is a family-owned eatery specializing in hearty Polish and Eastern European comfort foods (such as goulash, potato pancakes, and blintzes)
 <br />
-&bull; <a href="http://www.losburritostapatios.com">Los Burritos Tapatios</a> in Bolingbrook is a solid choice for over-stuffed burritos and cheap eats
+&bull; <a target="_blank" href="http://www.losburritostapatios.com">Los Burritos Tapatios</a> in Bolingbrook is a solid choice for over-stuffed burritos and cheap eats
 <br />
-&bull; <a href="http://www.magogrill.com">Mago Grill and Cantina</a> at The Promenade in Bolingbrook is a more upscale option for Latin and Mexican cuisine such as freshly smashed tableside guacamole, seafood ceviches, and suiza-style Burritos, Enchiladas, and Fajitas
+&bull; <a target="_blank" href="http://www.magogrill.com">Mago Grill and Cantina</a> at The Promenade in Bolingbrook is a more upscale option for Latin and Mexican cuisine such as freshly smashed tableside guacamole, seafood ceviches, and suiza-style Burritos, Enchiladas, and Fajitas
 <br />
-&bull; <a href="http://www.minghincuisine.com/locations">MingHin Cuisine Naperville</a>: A great place for traditional Chinese dim sum
+&bull; <a target="_blank" href="http://www.minghincuisine.com/locations">MingHin Cuisine Naperville</a>: A great place for traditional Chinese dim sum
 <br />
 &nbsp;
 <br />
 <h4>Check it Out</h4>
-&bull;  Saturday, Sept. 2nd happens to be the annual <a href="http://www.lemont.il.us/calendar.aspx?view=list&amp;year=2017&amp;month=9&amp;day=2">Lemont Heritage Fest</a>! There will be a parade at 2 pm in downtown Lemont, followed by food, family fun, and entertainment! Eric and I had a great time there last year in the historic downtown area.
+&bull;  Saturday, Sept. 2nd happens to be the annual <a target="_blank" href="http://www.lemont.il.us/calendar.aspx?view=list&amp;year=2017&amp;month=9&amp;day=2">Lemont Heritage Fest</a>! There will be a parade at 2 pm in downtown Lemont, followed by food, family fun, and entertainment! Eric and I had a great time there last year in the historic downtown area.
 <br />
-&bull; <a href="https://www.shoppingpromenade.com/">The Promenade Bolingbrook</a>: This outdoor mall is perfect for window-shopping or grabbing a bite to eat.
+&bull; <a target="_blank" href="https://www.shoppingpromenade.com/">The Promenade Bolingbrook</a>: This outdoor mall is perfect for window-shopping or grabbing a bite to eat.
 <br />
-&bull; <a href="http://www.napersettlement.org/index.aspx?NID=27">Naper Settlement</a>: This outdoor history museum in Naperville makes you feel like you are stepping back in time!
+&bull; <a target="_blank" href="http://www.napersettlement.org/index.aspx?NID=27">Naper Settlement</a>: This outdoor history museum in Naperville makes you feel like you are stepping back in time!
 <br />
-&bull; <a href="http://www.mortonarb.org/">The Morton Arboretum</a>: We love taking a break from the hustle and bustle of Chicago to visit this peaceful arboretum in Lisle, IL. It is home to hundreds of thousands of live plants, and is a perfect place for a leisurely nature walk.
+&bull; <a target="_blank" href="http://www.mortonarb.org/">The Morton Arboretum</a>: We love taking a break from the hustle and bustle of Chicago to visit this peaceful arboretum in Lisle, IL. It is home to hundreds of thousands of live plants, and is a perfect place for a leisurely nature walk.
 <br />
 &nbsp;
 <br />
@@ -82,25 +82,25 @@ If you have time to explore Chicago before heading to the suburbs, here are some
 <h4>Where to Eat</h4>
 We live in Chicago and we LOVE to eat out. We&rsquo;ve listed a few of our favorite restaurants below. If you&rsquo;re looking for recommendations for a specific type of cuisine or in a specific neighborhood, let us know and we&rsquo;ll send some suggestions.
 <br />
-&bull; <a href="http://www.portillos.com/index.html">Portillos</a>: &nbsp;A Chicago staple, this fast food chain offers delicious Chicago style hot dogs, fries, and Italian beef sandwiches.
+&bull; <a target="_blank" href="http://www.portillos.com/index.html">Portillos</a>: &nbsp;A Chicago staple, this fast food chain offers delicious Chicago style hot dogs, fries, and Italian beef sandwiches.
 <br />
-&bull; <a href="https://www.quartinochicago.com/">Quartino</a>: Located right downtown, this is a bustling small plates Italian restaurant with affordable dishes and great ambiance.
+&bull; <a target="_blank" href="https://www.quartinochicago.com/">Quartino</a>: Located right downtown, this is a bustling small plates Italian restaurant with affordable dishes and great ambiance.
 <br />
-&bull; <a href="http://shawscrabhouse.com/chicago/">Shaws Crab House</a>: This classy restaurant has stellar seafood and has an old school feel, perfect for a date night.
+&bull; <a target="_blank" href="http://shawscrabhouse.com/chicago/">Shaws Crab House</a>: This classy restaurant has stellar seafood and has an old school feel, perfect for a date night.
 <br />
-&bull; <a href="http://www.imperial-lamian.com/#brunch-1">Imperial Lamian</a>: This restaurant features Hong Kong style soup dumplings, which are delicious!
+&bull; <a target="_blank" href="http://www.imperial-lamian.com/#brunch-1">Imperial Lamian</a>: This restaurant features Hong Kong style soup dumplings, which are delicious!
 <br />
 &nbsp;
 <br />
 <h4>Check it Out</h4>
 <br />
-&bull; Hancock Tower: The Hancock Tower has amazing views of the city. Instead of taking the formal tour, take the elevator to <a href="https://www.signatureroom.com/lounge/">the bar</a> on the 96th floor and take in the view while sipping a cocktail. Ladies, the best views of Chicago are from the women&rsquo;s restroom!
+&bull; Hancock Tower: The Hancock Tower has amazing views of the city. Instead of taking the formal tour, take the elevator to <a target="_blank" href="https://www.signatureroom.com/lounge/">the bar</a> on the 96th floor and take in the view while sipping a cocktail. Ladies, the best views of Chicago are from the women&rsquo;s restroom!
 <br />
-&bull; <a href="http://chicago.cubs.mlb.com/chc/ballpark/">Wrigley Field</a>
+&bull; <a target="_blank" href="http://chicago.cubs.mlb.com/chc/ballpark/">Wrigley Field</a>
 <br />
-&bull; <a href="http://www.lpzoo.org/">Lincoln Park Zoo</a>: A free zoo located just north of downtown. It has great views of downtown and a brand new polar bear exhibit.
+&bull; <a target="_blank" href="http://www.lpzoo.org/">Lincoln Park Zoo</a>: A free zoo located just north of downtown. It has great views of downtown and a brand new polar bear exhibit.
 <br />
-&bull; <a href="https://www.themagnificentmile.com/">Chicago&rsquo;s Magnificent Mile</a>: North Michigan Ave in the heart of Chicago is famous for its shopping and people watching!&nbsp;
+&bull; <a target="_blank" href="https://www.themagnificentmile.com/">Chicago&rsquo;s Magnificent Mile</a>: North Michigan Ave in the heart of Chicago is famous for its shopping and people watching!&nbsp;
         </div>
 	</div>
 
