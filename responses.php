971cbb9dd3442707d5a3f9954bc96517a627c909
Brian Lam <brian@brianlam.me>
2017-05-10 22:20:22 -0700

    check count before showing response fields

--- responses.php
+++ responses.php
@@ -14,37 +14,55 @@
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
+            $count = $_POST["count"];
+
             displayRow($row, "count");
             displayRow($row, "name1");
             displayRow($row, "attending1");
             displayRow($row, "menu1");
             displayRow($row, "shuttle1");
-            displayRow($row, "comments1");
             displayRow($row, "dietary1");
-            displayRow($row, "name2");
-            displayRow($row, "attending2");
-            displayRow($row, "menu2");
-            displayRow($row, "shuttle2");
-            displayRow($row, "comments2");
-            displayRow($row, "dietary2");
-            displayRow($row, "name3");
-            displayRow($row, "attending3");
-            displayRow($row, "menu3");
-            displayRow($row, "shuttle3");
-            displayRow($row, "comments3");
-            displayRow($row, "dietary3");
-            displayRow($row, "name4");
-            displayRow($row, "attending4");
-            displayRow($row, "menu4");
-            displayRow($row, "shuttle4");
-            displayRow($row, "comments4");
-            displayRow($row, "dietary4");
-            displayRow($row, "name5");
-            displayRow($row, "attending5");
-            displayRow($row, "menu5");
-            displayRow($row, "shuttle5");
-            displayRow($row, "comments5");
-            displayRow($row, "dietary5");
+            displayRow($row, "comments1");
+
+            if ($count >= 2) 
+            {
+                displayRow($row, "name2");
+                displayRow($row, "attending2");
+                displayRow($row, "menu2");
+                displayRow($row, "shuttle2");
+                displayRow($row, "dietary2");
+                displayRow($row, "comments2");
+            }
+
+            if ($count >= 3) 
+            {
+                displayRow($row, "name3");
+                displayRow($row, "attending3");
+                displayRow($row, "menu3");
+                displayRow($row, "shuttle3");
+                displayRow($row, "dietary3");
+                displayRow($row, "comments3");
+            }
+            
+            if ($count >= 4) 
+            {
+                displayRow($row, "name4");
+                displayRow($row, "attending4");
+                displayRow($row, "menu4");
+                displayRow($row, "shuttle4");
+                displayRow($row, "dietary4");
+                displayRow($row, "comments4");
+            }
+
+            if ($count >= 5) 
+            {
+                displayRow($row, "name5");
+                displayRow($row, "attending5");
+                displayRow($row, "menu5");
+                displayRow($row, "shuttle5");
+                displayRow($row, "dietary5");
+                displayRow($row, "comments5");
+            }
             echo "<hr>";
         }
     } else {
