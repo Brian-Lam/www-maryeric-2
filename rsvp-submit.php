dce88fa0f31a9a4a0e8e38869d54114a59307fe7
Brian Lam <brian@brianlam.me>
2017-05-10 21:34:34 -0700

    default field value if not set

--- rsvp-submit.php
+++ rsvp-submit.php
@@ -11,6 +11,17 @@
         exit;
     }
 
+    function getResponseField($fieldname)
+    {
+        if (isset($_POST[$fieldname]))
+        {
+            return $_POST[$fieldname];
+        }
+
+        return "";
+
+    }
+
     // Required fields
     $required_fields = array("name-1", "attending-1", "menu-1", "shuttle-1");
 
@@ -39,42 +50,42 @@
     $stmt->bind_param("isssssssssssssssssssssssss", $count, $name1, $attending1, $menu1, $shuttle1, $comments1, $dietary1, $name2, $attending2, $menu2, $shuttle2, $comments2, $dietary2, $name3, $attending3, $menu3, $shuttle3, $comments3, $dietary3, $name4, $attending4, $menu4, $shuttle4, $comments4, $dietary4, $name5, $attending5, $menu5, $shuttle5, $comments5, $dietary5);
 
     // set parameters and execute
-    $count = $_POST["partycount"];
+    $count = getResponseField("partycount");
 
-    $name1 = $_POST["name-1"];
-    $attending1 = $_POST["attending-1"];
-    $menu1 = $_POST["menu-1"];
-    $shuttle1 = $_POST["shuttle-1"];
-    $comments1 = $_POST["comments-1"];
-    $dietary1 = $_POST["dietary-1"];
+    $name1 = getResponseField("name-1");
+    $attending1 = getResponseField("attending-1");
+    $menu1 = getResponseField("menu-1");
+    $shuttle1 = getResponseField("shuttle-1");
+    $comments1 = getResponseField("comments-1");
+    $dietary1 = getResponseField("dietary-1");
 
-    $name2 = $_POST["name-2"];
-    $attending2 = $_POST["attending-2"];
-    $menu2 = $_POST["menu-2"];
-    $shuttle2 = $_POST["shuttle-2"];
-    $comments2 = $_POST["comments-2"];
-    $dietary2 = $_POST["dietary-2"];
+    $name2 = getResponseField("name-2");
+    $attending2 = getResponseField("attending-2");
+    $menu2 = getResponseField("menu-2");
+    $shuttle2 = getResponseField("shuttle-2");
+    $comments2 = getResponseField("comments-2");
+    $dietary2 = getResponseField("dietary-2");
 
-    $name3 = $_POST["name-3"];
-    $attending3 = $_POST["attending-3"];
-    $menu3 = $_POST["menu-3"];
-    $shuttle3 = $_POST["shuttle-3"];
-    $comments3 = $_POST["comments-3"];
-    $dietary3 = $_POST["dietary-3"];
+    $name3 = getResponseField("name-3");
+    $attending3 = getResponseField("attending-3");
+    $menu3 = getResponseField("menu-3");
+    $shuttle3 = getResponseField("shuttle-3");
+    $comments3 = getResponseField("comments-3");
+    $dietary3 = getResponseField("dietary-3");
 
-    $name4 = $_POST["name-4"];
-    $attending4 = $_POST["attending-4"];
-    $menu4 = $_POST["menu-4"];
-    $shuttle4 = $_POST["shuttle-4"];
-    $comments4 = $_POST["comments-4"];
-    $dietary4 = $_POST["dietary-4"];
+    $name4 = getResponseField("name-4");
+    $attending4 = getResponseField("attending-4");
+    $menu4 = getResponseField("menu-4");
+    $shuttle4 = getResponseField("shuttle-4");
+    $comments4 = getResponseField("comments-4");
+    $dietary4 = getResponseField("dietary-4");
 
-    $name5 = $_POST["name-5"];
-    $attending5 = $_POST["attending-5"];
-    $menu5 = $_POST["menu-5"];
-    $shuttle5 = $_POST["shuttle-5"];
-    $comments5 = $_POST["comments-5"];
-    $dietary5 = $_POST["dietary-5"];
+    $name5 = getResponseField("name-5");
+    $attending5 = getResponseField("attending-5");
+    $menu5 = getResponseField("menu-5");
+    $shuttle5 = getResponseField("shuttle-5");
+    $comments5 = getResponseField("comments-5");
+    $dietary5 = getResponseField("dietary-5");
 
     $stmt->execute();
     $stmt->close();
